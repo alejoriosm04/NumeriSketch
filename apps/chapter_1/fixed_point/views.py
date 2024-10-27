401991169df802687d172cc3da8648ad65e0fb10
josetor101 <josetordecilla800@gmail.com>
2024-10-27 18:04:01 -0500

    .

--- apps/chapter_1/fixed_point/views.py
+++ apps/chapter_1/fixed_point/views.py
@@ -1,23 +1,25 @@
+import os
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+import numpy as np
+import matplotlib.pyplot as plt
 import math
 import pandas as pd
 import matplotlib
 matplotlib.use('Agg')  # Usar el backend Agg para evitar problemas con la GUI
-import matplotlib.pyplot as plt
-import numpy as np
-from django.shortcuts import render
-from django.http import HttpResponseRedirect
-import os
 
 
 def generate_graph(x0, fx, gx, s, safe_dict):
     x_vals = np.linspace(x0 - 2, x0 + 2, 400)
-    f_vals = [eval(fx, {"x": val, "math": math, "__builtins__": {}}, safe_dict) for val in x_vals]
-    #gx_vals = [eval(gx, {"x": valg, "math": math, "__builtins__": {}}, safe_dict) for valg in x_vals]
+    f_vals = [eval(fx, {"x": val, "math": math,
+                   "__builtins__": {}}, safe_dict) for val in x_vals]
+    # gx_vals = [eval(gx, {"x": valg, "math": math, "__builtins__": {}}, safe_dict) for valg in x_vals]
 
     plt.figure(figsize=(10, 6))
     plt.plot(x_vals, f_vals, label='f(x)')
-    #plt.plot(x_vals, gx_vals, label='g(x)')
-    plt.scatter([s], [eval(fx, {"x": s, "math": math, "__builtins__": {}}, safe_dict)], color='red', zorder=5, label='Solution Point')
+    # plt.plot(x_vals, gx_vals, label='g(x)')
+    plt.scatter([s], [eval(fx, {"x": s, "math": math, "__builtins__": {
+                }}, safe_dict)], color='red', zorder=5, label='Solution Point')
     plt.title('Graph of f(x)')
     plt.xlabel('x')
     plt.ylabel('y')
@@ -33,8 +35,7 @@ def generate_graph(x0, fx, gx, s, safe_dict):
     graph_path = os.path.join(graph_dir, 'graph.png')
     plt.savefig(graph_path)
 
-
-    #return os.path.join('graphs', 'graph.png') 
+    # return os.path.join('graphs', 'graph.png')
 
     # Save the figure in SVG format
     svg_path = os.path.join(graph_dir, 'graph.svg')
@@ -42,12 +43,11 @@ def generate_graph(x0, fx, gx, s, safe_dict):
 
     plt.close()
 
-
     return {
         'png': os.path.join('graphs', 'graph.png'),
         'svg': os.path.join('graphs', 'graph.svg')
     }
-    
+
 
 def fixed_point(request):
     context = {
@@ -56,7 +56,7 @@ def fixed_point(request):
         'niter': 0,
         'fx': '',
         'gx': '',
-        'error_type': 'absoluto',  # Valor por defecto
+        'error_type': 'absolute',  # Valor por defecto
         'msg': [],
         'table': None,
         'error': True,
@@ -72,8 +72,8 @@ def fixed_point(request):
         'log': math.log,
         'exp': math.exp,
         'sqrt': math.sqrt,
-        'abs': abs,  
-        'factorial': math.factorial, 
+        'abs': abs,
+        'factorial': math.factorial,
         'asin': math.asin,
         'acos': math.acos,
         'atan': math.atan,
@@ -94,11 +94,13 @@ def fixed_point(request):
             context['niter'] = int(request.POST.get('niter'))
             context['fx'] = request.POST.get('fx').replace('^', '**')
             context['gx'] = request.POST.get('gx').replace('^', '**')
-            context['error_type'] = request.POST.get('error_type')  # Obtener el tipo de error seleccionado
+            # Obtener el tipo de error seleccionado
+            context['error_type'] = request.POST.get('error_type')
 
             x = context['x0']
-            f = eval(context['fx'], {"x": x, "math": math, "__builtins__": {}}, safe_dict)
-            fn = [] 
+            f = eval(context['fx'], {
+                     "x": x, "math": math, "__builtins__": {}}, safe_dict)
+            fn = []
             xn = []
             E = []
             N = []
@@ -111,19 +113,20 @@ def fixed_point(request):
             N.append(c)
             s = None
 
-            while Error > context['tol'] and f != 0 and c < context['niter']:
-                x = eval(context['gx'], {"x": x, "math": math, "__builtins__": {}}, safe_dict)
-                fe = eval(context['fx'], {"x": x, "math": math, "__builtins__": {}}, safe_dict)
+            while Error > context['tol'] and fe != 0 and c < context['niter']:
+                x = eval(context['gx'], {
+                         "x": x, "math": math, "__builtins__": {}}, safe_dict)
+                fe = eval(context['fx'], {
+                          "x": x, "math": math, "__builtins__": {}}, safe_dict)
 
                 fn.append(fe)
                 xn.append(x)
-                c = c + 1
+                c +=1
 
-                # Calcular el error dependiendo del tipo seleccionado
-                if context['error_type'] == "relativo":
-                    Error = abs((xn[c] - xn[c-1]) / xn[c])
-                else:
-                    Error = abs(xn[c] - xn[c-1])
+                if context['error_type'] == "relative":  
+                    Error = abs((xn[c] - xn[c - 1]) / xn[c])
+                else:  # Cálculo del error absoluto
+                    Error = abs(xn[c] - xn[c - 1])
 
                 N.append(c)
                 E.append(Error)
@@ -136,12 +139,9 @@ def fixed_point(request):
             }
             df = pd.DataFrame(data)
             context['table'] = df.to_dict(orient='records')
+            print("Table: ", context['table'])
 
-            if(f == 0):
-                s=0
-                context['error'] = False
-                context['msg'].append(f"0 es una solución para {str(context['fx'])}")
-            elif fe == 0:
+            if fe == 0:
                 s = x
                 context['msg'].append(f"{s} es raíz de f(x)")
                 context['error'] = False
@@ -153,7 +153,7 @@ def fixed_point(request):
             else:
                 context['msg'].append(f"Fracaso en {context['niter']} iteraciones.")
                 
-            if(s != None):  
+            if(s is not None):  
                 context['graph'] = generate_graph(context['x0'], context['fx'], context['gx'], s, safe_dict)
 
         except Exception as e:
